b590fcb7dcf5f3845a364edecd8b40dd92671c0e
Michel Barnich <private@michelbarnich.com>
2020-04-01 15:24:42 +0200

    Initial Commit

---
+++ Themey CLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Themey CLI
+//
+//  Created by Michel Barnich on 01/04/2020.
+//  Copyright © 2020 Michel Barnich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
