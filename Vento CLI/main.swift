9ae0f14c488818e39a1d0ea103618539dddfe287
Michel Barnich <private@michelbarnich.com>
2020-04-10 09:09:21 +0200

    Version 1.0

---
+++ Vento CLI/main.swift
@@ -0,0 +1,71 @@
+//
+//  main.swift
+//  vento CLI
+//
+//  Created by Michel Barnich on 01/04/2020.
+//  Copyright © 2020 Michel Barnich. All rights reserved.
+//
+
+import Foundation
+
+print("""
+-------------------------------
+|          Vento CLI          |
+|    made by @michelbarnich   |
+-------------------------------
+This sowftware is meant for development purposes only! You are not allowed to share this software with 3rd parties! Even though it is very unlikely, I'm not responsible for any damage that might be caused by using vento or vento CLI.
+
+usage:
+-b  print Bundle Identifiers of Apps in "/Applications" Use this if a specific Icon doesnt show up, maybe the Info.plist of the Application messes up the "XML Parser"
+
+-a  prints out the App Icon Names. No specific usage to be honest.
+
+-i  installing a theme, the next argument has to be the path of the folder containing the theme!
+
+-e  export theme. This will take the current Icons of the Apps, and compile a theme out of them. This is used to create a backup of the original icons. The backup Theme can be found on your Desktop. It will create a folder named "vento_backup"
+------------------------------
+""")
+
+if CommandLine.arguments.contains("-b") {
+    print("[INFO:] printing Bundle Identifiers of Apps installed in \"/Applications\"")
+    
+    let installedAppsInfo = getInstalledAppsInfoArray()
+    
+    for appArray in installedAppsInfo {
+        print("[INFO:] \(appArray[0]): \(getBundleIdentifierOfApplication(path: appArray[1]))")
+    }
+}
+
+if CommandLine.arguments.contains("-i") {
+    
+    if CommandLine.arguments.firstIndex(of: "i") != CommandLine.arguments.count {
+        installTheme(themeFolderPath: CommandLine.arguments[CommandLine.arguments.firstIndex(of: "-i")! + 1])
+    }
+    
+}
+
+if CommandLine.arguments.contains("-a") {
+    print("[INFO:] printing Icon Names of Apps installed in \"/Applications\"")
+    
+    let installedAppsInfo = getInstalledAppsInfoArray()
+    
+    for appArray in installedAppsInfo {
+        print("[INFO:] \(appArray[0]): \(getApplicationIconName(path: appArray[1]))")
+    }
+}
+
+if CommandLine.arguments.contains("-e") {
+    backupCurrentTheme()
+}
+
+extension String {
+
+    func slice(from: String, to: String) -> String? {
+
+        return (range(of: from)?.upperBound).flatMap { substringFrom in
+            (range(of: to, range: substringFrom..<endIndex)?.lowerBound).map { substringTo in
+                String(self[substringFrom..<substringTo])
+            }
+        }
+    }
+}
---
+++ Vento CLI/scripts/Vento.swift
@@ -0,0 +1,104 @@
+//
+//  Vento.swift
+//  Vento CLI
+//
+//  Created by Michel Barnich on 01/04/2020.
+//  Copyright © 2020 Michel Barnich. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+func getInstalledAppsInfoArray() -> Array<Array<String>> {
+    var installedAppsArray = [[String]]()
+    
+    do {
+        let installedApps = try FileManager.default.contentsOfDirectory(atPath: "/Applications")
+        
+        for app in installedApps {
+            
+            if app.hasSuffix(".app") {
+                installedAppsArray.append([app, "/Applications/\(app)"])
+            }
+        }
+        
+        return installedAppsArray
+    } catch {
+        return installedAppsArray
+    }
+}
+
+func backupCurrentTheme() {
+    print("[INFO:] creating Backup of current theme...")
+    if(!FileManager.default.fileExists(atPath: "/Users/\(NSUserName())/Desktop/vento_backup")) {
+        
+        do {
+            try FileManager.default.createDirectory(atPath: "/Users/\(NSUserName())/Desktop/vento_backup", withIntermediateDirectories: false, attributes: nil)
+            
+            for appInfo in getInstalledAppsInfoArray() {
+                var iconName = getApplicationIconName(path: appInfo[1])
+                
+                if(!iconName.hasSuffix(".icns")) {
+                    iconName = iconName + ".icns"
+                }
+                
+                if(FileManager.default.fileExists(atPath: "/Applications/\(appInfo[0])/Contents/Resources/\(iconName)") && !FileManager.default.fileExists(atPath: "/Users/\(NSUserName())/Desktop/vento_backup/\(getBundleIdentifierOfApplication(path: appInfo[1])).icns")) {
+                    
+                    print("[INFO:] backing up \(getBundleIdentifierOfApplication(path: appInfo[1]))")
+                    
+                    try FileManager.default.copyItem(at: URL(fileURLWithPath: "/Applications/\(appInfo[0])/Contents/Resources/\(iconName)"), to: URL(fileURLWithPath: "/Users/\(NSUserName())/Desktop/vento_backup/\(getBundleIdentifierOfApplication(path: appInfo[1])).icns"))
+                    
+                }
+            }
+            
+        } catch {
+            print("[ERROR:] \(error)")
+        }
+        
+    }
+    
+}
+
+func installTheme(themeFolderPath: String) {
+    backupCurrentTheme()
+    
+    print("[INFO:] installing Theme")
+    
+    var themeFolderPathCorrected = themeFolderPath
+    if !themeFolderPath.hasSuffix("/") {
+        themeFolderPathCorrected += "/"
+    }
+    
+    let appInfoArray = getInstalledAppsInfoArray()
+    
+    for app in appInfoArray {
+        
+        do {
+            
+            var appIconName = getApplicationIconName(path: app[1])
+            if !appIconName.hasSuffix(".icns") {
+                appIconName = appIconName + ".icns"
+            }
+            
+            let expectedIconPath = themeFolderPathCorrected + getBundleIdentifierOfApplication(path: app[1]) + ".icns"
+            
+            if FileManager.default.fileExists(atPath: expectedIconPath) {
+                
+                print("[INFO:] copying Icon for \(app[0])")
+                
+                NSWorkspace.shared.setIcon(NSImage(byReferencing: URL(fileURLWithPath: expectedIconPath)), forFile: "/Applications/\(app[0])", options: NSWorkspace.IconCreationOptions(rawValue: 0))
+                
+                var AppURL = URL(fileURLWithPath: "/Applications/\(app[0])")
+                var InfoURL = URL(fileURLWithPath: "/Applications/\(app[0])/Info.plist")
+                var resourceValues = URLResourceValues()
+                resourceValues.contentModificationDate = Date()
+                try? AppURL.setResourceValues(resourceValues)
+                try? InfoURL.setResourceValues(resourceValues)
+            }
+            
+        } catch {
+            print("[ERROR:] \(error)")
+        }
+    }
+    
+}
